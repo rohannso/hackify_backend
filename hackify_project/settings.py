18626ffadb995275f8c7b61d5567ac2e2ab9fc8c
rohannso <160991416+rohannso@users.noreply.github.com>
2026-04-05 00:01:12 +0530

    Update settings.py

--- hackify_project/settings.py
+++ hackify_project/settings.py
@@ -161,11 +161,7 @@ CORS_ALLOWED_ORIGINS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST =  'smtp.gmail.com'
 EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
-<<<<<<< HEAD
 EMAIL_USE_TLS = True
-=======
-EMAIL_USE_TLS = True 
->>>>>>> 1af0fd76c87d5fcd3821a4b493f4e3311596efe2
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', default='noreply@hackify.com')

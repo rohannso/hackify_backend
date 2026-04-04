2a5c7921917b692c5ceef14302929bfc16105b22
rohannso <160991416+rohannso@users.noreply.github.com>
2026-04-04 23:11:13 +0530

    Fix EMAIL_USE_TLS setting to use boolean value

--- hackify_project/settings.py
+++ hackify_project/settings.py
@@ -161,7 +161,7 @@ CORS_ALLOWED_ORIGINS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST =  'smtp.gmail.com'
 EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
-EMAIL_USE_TLS = 'True' 
+EMAIL_USE_TLS = True 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', default='noreply@hackify.com')

a176cd3787a90724c470d7137f90d1a0b9b1e0d0
Rohan Sonawane <rohannso@gmail.com>
2026-04-05 01:00:17 +0530

    upadted to send grid

--- hackify_project/settings.py
+++ hackify_project/settings.py
@@ -159,12 +159,13 @@ CORS_ALLOWED_ORIGINS = [
 
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST =  'smtp.gmail.com'
-EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', default='noreply@hackify.com')
+EMAIL_HOST_USER = 'apikey'  # Literally the string 'apikey' - don't change this!
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY', '')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'rohannso14@gmail.com')
+EMAIL_TIMEOUT = 10
 OTP_EXPIRY_MINUTES = 10
 print(EMAIL_HOST_USER," ",EMAIL_HOST_PASSWORD)
 
